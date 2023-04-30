79b5381dde67838f5287d9406675b041fa7d8b0e
avand555 <82321286+avand555@users.noreply.github.com>
2023-04-30 03:04:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ if (visitCount) {
   localStorage.setItem("page_view", visitCount);
 } else {
   visitCount = 1;
-  localStorage.setItem("page_view", 1);
+  localStorage.setItem("page_view", 2);
 }
 counterContainer.innerHTML = visitCount;
 
 // Adding onClick event listener
 resetButton.addEventListener("click", () => {
   visitCount = 1;
-  localStorage.setItem("page_view", 1);
+  localStorage.setItem("page_view", 2);
   counterContainer.innerHTML = visitCount;
 });
